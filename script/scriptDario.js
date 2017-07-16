17bb0009fc769b96d92089f95c5bb53857a72f42
Davide <dvdcsc@gmail.com>
2017-07-16 22:32:33 +0200

    [FIX] Migliorata la logica di comparsa del top menu [FIX] Aggiunta un'animazione di comparsa del top menu

--- script/scriptDario.js
+++ script/scriptDario.js
@@ -5,18 +5,22 @@ function menuShower() {
     var menu = $("#dario");
     var logo = $("#logoDario")
     var posizione = menu.position();
-
+    var headerH = $("#header").height();
+    var menuList = $("#menuList")
 
     $(window).scroll(function() {
-        if ($(window).scrollTop() >280+ posizione.top&&$( window ).width()>=751) {
+        if ($(window).scrollTop() >headerH*0.95 && $( window ).width()>=751) {
             menu.addClass("header__dario");
             menu.removeClass("header__darioHide");
-            //
+            menu.addClass("animate");
             logo.fadeTo( "slow", 1 );
+            menuList.fadeTo("slow", 1 );
 
         } else {
             menu.removeClass("header__dario");
             menu.addClass("header__darioHide");
+            menu.removeClass("animate");
+
             logo.css({'opacity':'0'});
             logo.stop(true,true);
         }

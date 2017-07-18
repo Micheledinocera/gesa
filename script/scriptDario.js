9c6f862638c4931a9d226fdd7598a8ecba936dda
Davide <dvdcsc@gmail.com>
2017-07-18 23:02:02 +0200

    Utilizzo di una sola lista per entrambi i menu, animazione testo da sistemare

--- script/scriptDario.js
+++ script/scriptDario.js
@@ -6,20 +6,29 @@ function menuShower() {
     var logo = $("#logoDario")
     var posizione = menu.position();
     var headerH = $("#header").height();
-    var menuList = $("#menuList")
+    var menuListMin = $("#menuList_min");
+    var menuList = $("#menuList");
+    var children = menuList.children();
 
     $(window).scroll(function() {
         if ($(window).scrollTop() >headerH*0.95 && $( window ).width()>=751) {
+
+            menu.addClass("animate");
+            menuListMin.append(children);
+
+            menuListMin.addClass("isOpen");
             menu.addClass("header__dario");
             menu.removeClass("header__darioHide");
-            menu.addClass("animate");
+            
             logo.fadeTo( "slow", 1 );
-            menuList.fadeTo("slow", 1 );
 
         } else {
+            menu.removeClass("animate");
+            menuList.append(children);
+
+            menuListMin.removeClass("isOpen");
             menu.removeClass("header__dario");
             menu.addClass("header__darioHide");
-            menu.removeClass("animate");
 
             logo.css({'opacity':'0'});
             logo.stop(true,true);
